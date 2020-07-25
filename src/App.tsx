a2c7916b2fcaf2d6b0d729514afde50f5fa6dec9
Marcin <cinekpl97@gmail.com>
2020-07-25 22:48:27 +0200

    Fixed scroll problem, added nested Media Component, fixed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import React from 'react';
-import TopNavbar from './components/navbar'
-import Background from './background.jpg'
-import './custom.css'
-import MediaBox from './components/mediaBox'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React from "react";
+import TopNavbar from "./components/navbar";
+import "./custom.css";
+import MediaBox from "./components/mediaBox";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function App(): JSX.Element {
   return (
-    <div className='container'>
+    <div className="container">
       <Container>
         <Row>
-          <Col>
+          <Col xl={12}>
             <TopNavbar></TopNavbar>
           </Col>
         </Row>
         <Row>
           <Col xl={2}></Col>
           <Col xl={8}>
-            <MediaBox></MediaBox>
+            <div className="desktop">
+              <MediaBox></MediaBox>
+            </div>
           </Col>
           <Col xl={2}></Col>
-
-
         </Row>
       </Container>
-
     </div>
   );
 }
--- src/components/mediaBox.tsx
+++ src/components/mediaBox.tsx
@@ -1,51 +1,156 @@
-import React from 'react';
-import Media from 'react-bootstrap/Media'
-import Image from 'react-bootstrap/Image'
-import '../custom.css'
-import Icon from '../iconForText.png'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import Media from "react-bootstrap/Media";
+import Image from "react-bootstrap/Image";
+import "../custom.css";
+import Icon from "../iconForText.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function MediaBox(): JSX.Element {
-    return (
-        <Media className='mediabox'>
-
-            <Container fluid>
-                <Row>
-                    <Col xs={12} xl={2}>
-                        <Image
-                            src={Icon}
-                            roundedCircle
-                            width={90}
-                            height={90}
-                            className="mediaBoxImage"
-
-                        ></Image>
-                    </Col>
-                    <Col xs={12} xl={10}>
-                        <Media.Body className="mediaBoxBody">
-                            <h5>Media Heading</h5>
-                            <p>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-                                Donec lacinia congue felis in faucibus.ddddddddddddddddddddddddddddddddd
-                                fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-                                ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-                                fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-                                ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
-                                ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffs
-                                ssssssssssssssssssssssssssssssssssssssssssssss
-                            </p>
-                        </Media.Body>
-                    </Col>
-                </Row>
-
-
-            </Container>
-
-        </Media>
-    );
+  return (
+    <Media className="mediabox">
+      <Container fluid>
+        <Row>
+          <Col xs={12} xl={2}>
+            <Image src={Icon} roundedCircle className="mediaBoxImage"></Image>
+          </Col>
+          <Col xs={12} xl={10}>
+            <Media.Body className="mediaBoxBody">
+              <h3>Pan Tadeusz</h3>
+              <h6>
+                Litwo! Ojczyzno maja! Ty jesteś jak zdrowie, Ile cię
+                trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś
+                piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po
+                tobie" Panno święta, co Jasnej bronisz Częstochowy I w Ostrej
+                świecisz Bramie!
+              </h6>
+            </Media.Body>
+          </Col>
+        </Row>
+        <Row>
+          <Media>
+            <Media.Body className="mediaBoxBody">
+              <p>
+                Inwokacja Litwo! Ojczyzno maja! Ty jesteś jak zdrowie, Ile cię
+                trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś
+                piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po
+                tobie" Panno święta, co Jasnej bronisz Częstochowy I w Ostrej
+                świecisz Bramie! Ty, co gród zamkowy Nowogródzki ochraniasz z
+                jego wiernym ludem! Jak mnie dziecko do zdrowia powróciłaś
+                cudem, (Gdy od płaczącej matki pod Twoją opiekę Ofiarowany,
+                martwą podniosłem powiekę I zaraz mogłem pieszo do Twych świątyń
+                progu Iść za wrócone życie podziękować Bogu), Tak nas powrócisz
+                cudem na Ojczyzny łono. Tymczasem przenoś moją duszę utęsknioną
+                Do tych pagórków leśnych, do tych łąk zielonych, Szeroko nad
+                błękitnym Niemnem rozciągnionych; Do tych pól malowanych zbożem
+                rozmaitem, Wyzłacanych pszenicą, posrebrzanych żytem; Gdzie
+                bursztynowy świerzop, gryka jak śnieg biała, Gdzie panieńskim
+                rumieńcem dzięcielina pała, A wszystko przepasane jakby wstęgą,
+                miedzą Zieloną, na niej z rzadka ciche grusze siedzą.
+              </p>
+              <p>
+                Inwokacja stanowi liryczną zapowiedź wątków rozwijanych w
+                dalszych partiach poematu. Wskazuje także w pewien sposób na
+                genezę poematu, gdyż przedstawia jak gdyby sam początek procesu
+                twórczego. Pozwala również przyjąć odpowiednią postawę wobec
+                dzieła – wiemy, że będą to wspomnienia, a więc wydarzenia należy
+                postrzegać jako minione wobec sytuacji narracyjnej. Pozwala
+                także wywnioskować o czym i jak będzie w utworze mówione oraz
+                dostarcza pewnych wiadomości o podmiocie mówiącym, jak można się
+                domyślać, narratorze utworu.
+              </p>
+              <p>
+                Inwokacja Litwo! Ojczyzno maja! Ty jesteś jak zdrowie, Ile cię
+                trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś
+                piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po
+                tobie" Panno święta, co Jasnej bronisz Częstochowy I w Ostrej
+                świecisz Bramie! Ty, co gród zamkowy Nowogródzki ochraniasz z
+                jego wiernym ludem! Jak mnie dziecko do zdrowia powróciłaś
+                cudem, (Gdy od płaczącej matki pod Twoją opiekę Ofiarowany,
+                martwą podniosłem powiekę I zaraz mogłem pieszo do Twych świątyń
+                progu Iść za wrócone życie podziękować Bogu), Tak nas powrócisz
+                cudem na Ojczyzny łono. Tymczasem przenoś moją duszę utęsknioną
+                Do tych pagórków leśnych, do tych łąk zielonych, Szeroko nad
+                błękitnym Niemnem rozciągnionych; Do tych pól malowanych zbożem
+                rozmaitem, Wyzłacanych pszenicą, posrebrzanych żytem; Gdzie
+                bursztynowy świerzop, gryka jak śnieg biała, Gdzie panieńskim
+                rumieńcem dzięcielina pała, A wszystko przepasane jakby wstęgą,
+                miedzą Zieloną, na niej z rzadka ciche grusze siedzą.
+              </p>
+              <p>
+                Inwokacja stanowi liryczną zapowiedź wątków rozwijanych w
+                dalszych partiach poematu. Wskazuje także w pewien sposób na
+                genezę poematu, gdyż przedstawia jak gdyby sam początek procesu
+                twórczego. Pozwala również przyjąć odpowiednią postawę wobec
+                dzieła – wiemy, że będą to wspomnienia, a więc wydarzenia należy
+                postrzegać jako minione wobec sytuacji narracyjnej. Pozwala
+                także wywnioskować o czym i jak będzie w utworze mówione oraz
+                dostarcza pewnych wiadomości o podmiocie mówiącym, jak można się
+                domyślać, narratorze utworu.
+              </p>
+              <p>
+                Inwokacja Litwo! Ojczyzno maja! Ty jesteś jak zdrowie, Ile cię
+                trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś
+                piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po
+                tobie" Panno święta, co Jasnej bronisz Częstochowy I w Ostrej
+                świecisz Bramie! Ty, co gród zamkowy Nowogródzki ochraniasz z
+                jego wiernym ludem! Jak mnie dziecko do zdrowia powróciłaś
+                cudem, (Gdy od płaczącej matki pod Twoją opiekę Ofiarowany,
+                martwą podniosłem powiekę I zaraz mogłem pieszo do Twych świątyń
+                progu Iść za wrócone życie podziękować Bogu), Tak nas powrócisz
+                cudem na Ojczyzny łono. Tymczasem przenoś moją duszę utęsknioną
+                Do tych pagórków leśnych, do tych łąk zielonych, Szeroko nad
+                błękitnym Niemnem rozciągnionych; Do tych pól malowanych zbożem
+                rozmaitem, Wyzłacanych pszenicą, posrebrzanych żytem; Gdzie
+                bursztynowy świerzop, gryka jak śnieg biała, Gdzie panieńskim
+                rumieńcem dzięcielina pała, A wszystko przepasane jakby wstęgą,
+                miedzą Zieloną, na niej z rzadka ciche grusze siedzą.
+              </p>
+              <p>
+                Inwokacja stanowi liryczną zapowiedź wątków rozwijanych w
+                dalszych partiach poematu. Wskazuje także w pewien sposób na
+                genezę poematu, gdyż przedstawia jak gdyby sam początek procesu
+                twórczego. Pozwala również przyjąć odpowiednią postawę wobec
+                dzieła – wiemy, że będą to wspomnienia, a więc wydarzenia należy
+                postrzegać jako minione wobec sytuacji narracyjnej. Pozwala
+                także wywnioskować o czym i jak będzie w utworze mówione oraz
+                dostarcza pewnych wiadomości o podmiocie mówiącym, jak można się
+                domyślać, narratorze utworu.
+              </p>
+              <p>
+                Inwokacja Litwo! Ojczyzno maja! Ty jesteś jak zdrowie, Ile cię
+                trzeba cenić, ten tylko się dowie, Kto cię stracił. Dziś
+                piękność twą w całej ozdobie Widzę i opisuję, bo tęsknię po
+                tobie" Panno święta, co Jasnej bronisz Częstochowy I w Ostrej
+                świecisz Bramie! Ty, co gród zamkowy Nowogródzki ochraniasz z
+                jego wiernym ludem! Jak mnie dziecko do zdrowia powróciłaś
+                cudem, (Gdy od płaczącej matki pod Twoją opiekę Ofiarowany,
+                martwą podniosłem powiekę I zaraz mogłem pieszo do Twych świątyń
+                progu Iść za wrócone życie podziękować Bogu), Tak nas powrócisz
+                cudem na Ojczyzny łono. Tymczasem przenoś moją duszę utęsknioną
+                Do tych pagórków leśnych, do tych łąk zielonych, Szeroko nad
+                błękitnym Niemnem rozciągnionych; Do tych pól malowanych zbożem
+                rozmaitem, Wyzłacanych pszenicą, posrebrzanych żytem; Gdzie
+                bursztynowy świerzop, gryka jak śnieg biała, Gdzie panieńskim
+                rumieńcem dzięcielina pała, A wszystko przepasane jakby wstęgą,
+                miedzą Zieloną, na niej z rzadka ciche grusze siedzą.
+              </p>
+              <p>
+                Inwokacja stanowi liryczną zapowiedź wątków rozwijanych w
+                dalszych partiach poematu. Wskazuje także w pewien sposób na
+                genezę poematu, gdyż przedstawia jak gdyby sam początek procesu
+                twórczego. Pozwala również przyjąć odpowiednią postawę wobec
+                dzieła – wiemy, że będą to wspomnienia, a więc wydarzenia należy
+                postrzegać jako minione wobec sytuacji narracyjnej. Pozwala
+                także wywnioskować o czym i jak będzie w utworze mówione oraz
+                dostarcza pewnych wiadomości o podmiocie mówiącym, jak można się
+                domyślać, narratorze utworu.
+              </p>
+            </Media.Body>
+          </Media>
+        </Row>
+      </Container>
+    </Media>
+  );
 }
-
